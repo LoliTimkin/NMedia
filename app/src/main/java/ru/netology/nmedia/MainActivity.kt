23238f33f695e12ff0aff36da5a02ab6702f7ba2
Evgeny R <ryderi700@gmail.com>
2022-10-17 00:16:46 +0300

    added func convertTo

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -7,7 +7,7 @@ import ru.netology.nmedia.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -16,17 +16,18 @@ class MainActivity : AppCompatActivity() {
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
             published = "21 мая в 18:36",
-            likedByMe = false
+            likedByMe = false,
+            sharedByMe = true
         )
         binding.apply {
             author.text = post.author
             published.text = post.published
             content.text = post.content
-            post
+
             if (post.likedByMe) {
                 imageLikes?.setImageResource(R.drawable.ic_baseline_favorite_24)
             }
-            //likeCount?.text = post.likes.toString()
+            likesAmount?.text = post.likes.toString()
 
             //root.setOnClickListener {
             //    Log.d("stuff", "stuff")
@@ -44,11 +45,15 @@ class MainActivity : AppCompatActivity() {
                 )
 
                 if (post.likedByMe) post.likes++ else post.likes--
-                //likeCount?.text = post.likes.toString()
+                likesAmount?.text = post.likes.toString()
             }
 
+            sharesAmount.text = post.shared.toString()
+
             imageShare?.setOnClickListener {
-                post.shared = post.shared++
+                if (post.sharedByMe) post.shared++
+                sharesAmount?.text = ToConvert(post.shared)
+
             }
 
         }
@@ -59,25 +64,27 @@ class MainActivity : AppCompatActivity() {
         val content: String,
         val published: String,
         var likedByMe: Boolean,
+        var sharedByMe: Boolean,
         var likes: Int = 0,
-        var shared: Int = 0
+        var shared: Int = 995,
+
     )
 
 
-    fun ToK(count: Int) {
-        when (count) {
-            in 0..999 -> count
-            in 1_000..10_000 -> convert1(count)
-            in 10_000..1000_000 -> convert2(count)
-            else -> println("был(а) в сети давно")
+    fun ToConvert(count: Int): String  {
+        return when (count) {
+            in 0..999 -> count.toString()
+            in 1_000..10_000 ->  convert1K(count)
+            in 10_000..1000_000 ->  convert1M(count)
+            else ->  "значение вне диапозона"
         }
     }
 
-    fun convert1(count: Int) {
+    fun convert1K(count: Int): String {
         val thousands =  count / 1_000
         if (count % 1_000 > 0) {
             val hundredth = count - thousands * 1_000
-            when (hundredth) {
+            val decimal = when (hundredth) {
                 100 -> 1
                 200 -> 2
                 300 -> 3
@@ -87,16 +94,19 @@ class MainActivity : AppCompatActivity() {
                 700 -> 7
                 800 -> 8
                 900 -> 9
+                else ->  0
             }
-            println("$thousands,$hundredth K ")
+
+        return if (decimal != 0) "$thousands,$hundredth K" else "$thousands K"
+
         }
-        else println("$thousands K ")
+        else return "$thousands K"
 
     }
 
-    fun convert2 (count: Int) {
+    fun convert1M (count: Int): String {
         val millions: Int = count / 1_000_000
-        println("$millions K")
+        return "$millions K"
     }
 
 }
